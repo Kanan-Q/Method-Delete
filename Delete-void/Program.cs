7171b2075e5933175b72d504a9a6f781a7cabd23
Kanan-Q <kananag-bp215@code.edu.az>
2024-10-19 20:40:53 +0400

    Method delete

---
+++ Delete-void/Program.cs
@@ -0,0 +1,30 @@
+﻿using static System.Console;
+namespace Delete_void
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Hello World";
+            string result = Delete(sentence);
+            WriteLine(result);
+        }
+
+        static string Delete(string sentence)
+        {
+            string newSentence = "";
+
+            for (int i = 0; i < sentence.Length; i++)
+            {
+                if (sentence[i] != ' ')
+                {
+                    newSentence += sentence[i];
+                }
+            }
+
+            return newSentence;
+        }
+    }
+}
+
